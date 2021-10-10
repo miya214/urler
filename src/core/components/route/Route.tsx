601753ecb50655e3b873e2902ea5510873798e35
Yuuki Miyao <me110co62ca@gmail.com>
2021-10-10 15:17:19 +0900

    modified router

--- src/core/components/route/Route.tsx
+++ src/core/components/route/Route.tsx
@@ -29,7 +29,7 @@ import ScrollToTop from './ScrollToTop';
 import { MainElems } from './RouteElements';
 
 const RouteComponent: VFC = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <ScrollToTop />
     <Sidebar />
     <Navbar />
