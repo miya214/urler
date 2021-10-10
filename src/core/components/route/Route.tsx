f78e1ff39cce947ce3ef802079550b16a326a629
Yuuki Miyao <me110co62ca@gmail.com>
2021-10-10 16:35:50 +0900

    add webpack and index.html

--- src/core/components/route/Route.tsx
+++ src/core/components/route/Route.tsx
@@ -29,7 +29,7 @@ import ScrollToTop from './ScrollToTop';
 import { MainElems } from './RouteElements';
 
 const RouteComponent: VFC = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router basename="/urler">
     <ScrollToTop />
     <Sidebar />
     <Navbar />
