e3f5f488cc5d18e0d9d97b1c53b273acefc88d3a
Yuuki Miyao <me110co62ca@gmail.com>
2021-10-10 22:55:05 +0900

    modified index.html and 404.html

--- src/core/components/route/PrivateRoute.tsx
+++ src/core/components/route/PrivateRoute.tsx
@@ -14,6 +14,7 @@ import { PrivateRouteProps } from './types';
 const PrivateRoute: VFC<PrivateRouteProps> = ({ children, ...rest }) => {
   const location = useLocation();
   const isAuth = useSelector(selectIsAuth);
+  console.log(location);
 
   return (
     <Route
